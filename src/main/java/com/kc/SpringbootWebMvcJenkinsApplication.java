664ef45737b9573c5a8e00e0da397a7eb073e534
kishore0519 <kishorekoppolu1905@gmail.com>
2025-08-26 06:42:33 +0530

    added

---
+++ src/main/java/com/kc/SpringbootWebMvcJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.kc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebMvcJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebMvcJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kc/SpringbootWebMvcJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebMvcJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
