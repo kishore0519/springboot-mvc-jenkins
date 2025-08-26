080e8a8304addc69c82e7af2b61f111040ddc6cf
kishore0519 <kishorekoppolu1905@gmail.com>
2025-08-26 07:29:05 +0530

    Modified SimpleController to test the build

--- src/main/java/com/kc/rest/SimpleController.java
+++ src/main/java/com/kc/rest/SimpleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
 	
 	@GetMapping
-	public String saySomething() {
+	public String sayHi() {
 		return "Hi!!";
 	}
 
